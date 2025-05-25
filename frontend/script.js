019db52ce073bf3ef82e73a50aaffa76521d16de
bruhh22 <preethamshashi45@gmail.com>
2025-05-25 10:46:49 +0530

    small change in the url

--- frontend/script.js
+++ frontend/script.js
@@ -14,7 +14,7 @@ document.getElementById("linkForm").addEventListener("submit", async function (e
   loading.classList.remove("hidden");
 
   try {
-    const response = await fetch("https://summarizer-x8o5.onrender.com/", {
+    const response = await fetch("https://summarizer-x8o5.onrender.com/summarize", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
